8b8e959841877d412a06aae615d4cfd72d3a6bc2
mtech <kind.of.random@outlook.com>
2017-12-07 19:45:55 +0000

    landing page templates

--- ScoutsResourceCentre/LandingPage/views.py
+++ ScoutsResourceCentre/LandingPage/views.py
@@ -1,8 +1,8 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Scouts Resource Centre landing page.")
+    return render(request, 'LandingPage/home.html')
 
 def ph_users(request):
     return HttpResponse("Users will be coming soon.")
