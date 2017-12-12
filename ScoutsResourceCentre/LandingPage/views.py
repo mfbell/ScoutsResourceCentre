2441dcfba3bb384b93bb0048fbd9bbfb9fd30c6b
mtech <kind.of.random@outlook.com>
2017-12-12 19:41:15 +0000

    Testing

--- ScoutsResourceCentre/LandingPage/views.py
+++ ScoutsResourceCentre/LandingPage/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'LandingPage/home.html')
+    return render(request, 'LandingPage/landing.html')
 
 def ph_users(request):
     return HttpResponse("Users will be coming soon.")
