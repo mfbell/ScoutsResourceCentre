c827bed9e313b67aeff9bb09ff6f24a0a7e6df5e
mtech <kind.of.random@outlook.com>
2017-12-22 21:44:44 +0000

    model development

--- ScoutsResourceCentre/ResourceCentre/views.py
+++ ScoutsResourceCentre/ResourceCentre/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
+from . import models
+# Activity, Meeting, Camp, RiskAssessment
+
 
 def resources(request):
     return HttpResponse("Resource Centre landing page.")
@@ -8,9 +11,13 @@ def resources(request):
 def category(request, category):
     return HttpResponse("{} landing page.".format(category))
 
-def resource(request, external_id, slug, category):
-    return render(request, 'ResourceCentre/resource.html', {"title": slug, "description": external_id, "category": category})
-    #return HttpResponse("Resource {} landing page. {} {}".format(slug, external_id, category))
+def resource(request, category, external_id, slug):
+    if category == "activity":
+        resource = get_object_or_404(models.Activity, external_id=external_id)
+    #return render(request, 'ResourceCentre/resource.html', {"title": slug, "description": external_id, "category": category})
+        return HttpResponse("Resource {} landing page. {} {}\n{}".format(slug, external_id, category, dir(resource)))
+    else:
+        return HttpResponse("Unknown")
 
 def tree(request, pk):
     return HttpResponse("Resource {} relations tree view.".format(pk))
