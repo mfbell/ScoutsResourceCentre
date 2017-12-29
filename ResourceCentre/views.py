e3dd0176f3ce87561944c51704b9060c62619c0b
mtech <kind.of.random@outlook.com>
2017-12-29 21:16:58 +0000

    Work on the views

--- ResourceCentre/views.py
+++ ResourceCentre/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, Http404
+from django.http import Http404
 from django.views import View
 
 from . import models
@@ -18,14 +18,31 @@ class CategoryLandingView(View):
     """Resource category landing page."""
 
     def get(request, category):
+        # To add
+        # - search system? or suggested content
         return render(request, "ResourceCentre/category-landing.html", {"category": category})
+
+
+class ViewDivider:
+    """"""
+
+    def __init__(keyword, options, delete_kw=True):
+        self.keyword = keyword
+        self.options = options
+        self.delete_kw = delete_kw
+
+    def view(request, *args, **kwargs):
+        choice = kwargs[self.keyword]
+        if self.delete_kw:
+            del(kwargs[self.keyword])
+        return self.options[choice](request, *args, **kwargs)
 
 
 class ResourceView(View):
     """Processesing requests of detailed resources views."""
 
-    # Needs better design to decouple layers.
-    def get(self, request, category, external_id, slug):
+    # Need to create inheritied resource collection class/thing - tried but failed.
+    def get(request, category, external_id, slug):
         if category == "Activity":
             resource = get_object_or_404(models.Activity, external_id=external_id, slug=slug)
             template = "ResourceCentre/resources/activity.html"
@@ -40,7 +57,16 @@ class ResourceView(View):
             template = "ResourceCentre/resources/risk-assessment.html"
         else:
             raise Http404
-        # To add
-        # - record users' views
-        # - related resources
-        return render(request, template, {'resource': resource})
+        # View recording
+
+        # Related resource finder - AI project
+        related_resources = []
+        # Render
+        return render(request, template, {"resource": resource, "related": related_resources})
+
+
+class ResourceEditView(View):
+    """Resource editing view."""
+
+    def get(self, request, category, external_id, slug):
+        pass
