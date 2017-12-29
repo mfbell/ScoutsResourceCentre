b0b7a411010bdbda7d84742054c0c7ee5ac75333
mtech <kind.of.random@outlook.com>
2017-12-29 22:40:11 +0000

    Build out views

--- ResourceCentre/views.py
+++ ResourceCentre/views.py
@@ -5,6 +5,15 @@ from django.views import View
 from . import models
 
 
+def suggested_resources(user, quantity, category=[], **kwargs):
+    return []
+
+def related_resources(resource, quantity=3, category=[], **kwargs):
+    return []
+
+def view_recorder(users, resource, source):
+    pass
+
 class ResourceCentreLandingView(View):
     """Resource centre laning page view."""
 
@@ -24,14 +33,20 @@ class CategoryLandingView(View):
 
 
 class ViewDivider:
-    """"""
+    """Mutli-view selection handler."""
 
     def __init__(keyword, options, delete_kw=True):
+        """
+        keyword - kwarg name | string
+        options - key-value pair of kwarg value and view | dictionary
+        delete_kw - Delete keyword pair from kwargs
+
+        """
         self.keyword = keyword
         self.options = options
         self.delete_kw = delete_kw
 
-    def view(request, *args, **kwargs):
+    def __call__(request, *args, **kwargs):
         choice = kwargs[self.keyword]
         if self.delete_kw:
             del(kwargs[self.keyword])
@@ -39,35 +54,53 @@ class ViewDivider:
 
 
 class ActivityView(View):
+    template = "ResourceCentre/resources/activity.html"
+
     def get(request, external_id, slug):
         resource = get_object_or_404(models.Activity, external_id=external_id, slug=slug)
-        template = "ResourceCentre/resources/activity.html"
-        return render(request, template, {"resource": resource, "related": related_resources})
+        related = []
+        return render(request, self.template, {"resource": resource, "related": related_resources})
 
 
 class MeetingView(View):
+    template = "ResourceCentre/resources/meeting.html"
+
     def get(request, external_id, slug):
         resource = get_object_or_404(models.Meeting, external_id=external_id, slug=slug)
-        template = "ResourceCentre/resources/meeting.html"
-        return render(request, template, {"resource": resource, "related": related_resources})
+        related = []
+        return render(request, self.template, {"resource": resource, "related": related_resources})
 
 
 class CampView(View):
+    template = "ResourceCentre/resources/camp.html"
+
     def get(request, external_id, slug):
         resource = get_object_or_404(models.Camp, external_id=external_id, slug=slug)
-        template = "ResourceCentre/resources/camp.html"
-        return render(request, template, {"resource": resource, "related": related_resources})
+        related = []
+        return render(request, self.template, {"resource": resource, "related": related_resources})
 
 
 class RiskAssessmentView(View):
+    template = "ResourceCentre/resources/risk-assessment.html"
+
     def get(request, external_id, slug):
         resource = get_object_or_404(models.RiskAssessment, external_id=external_id, slug=slug)
-        template = "ResourceCentre/resources/risk-assessment.html"
-        return render(request, template, {"resource": resource, "related": related_resources})
+        related = []
+        return render(request, self.template, {"resource": resource, "related": related_resources})
 
 
-class ResourceEditView(View):
-    """Resource editing view."""
+resource_view_divider = ViewDivider(
+    "category",
+    {
+        "Activity": ActivityView.as_view(),
+        "Meeting": MeetingView.as_view(),
+        "Camp": CampView.as_view(),
+        "RiskAssessment": RiskAssessment.as_view()
+    }
+)
 
-    def get(self, request, category, external_id, slug):
-        pass
+#class ResourceEditView(View):
+#    """Resource editing view."""
+#
+#    def get(self, request, category, external_id, slug):
+#        pass
