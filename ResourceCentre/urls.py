172f4d086d4a04e582b7c8084aabcefb1e349848
mtech <kind.of.random@outlook.com>
2017-12-29 18:02:43 +0000

    Cleaned up urlconfig

--- ResourceCentre/urls.py
+++ ResourceCentre/urls.py
@@ -5,24 +5,24 @@ from . import views
 
 app_name = 'ResourceCentre'
 
-#resource_patterns = [
-#    url(r"^$", ),
-#    url(r"^/tree/$", views.tree, name="tree"),
-#]
+# To do
+# - Add sitemap
+# - Update to Django 2.0
 
-resource_by_pk_pattern = url(r"^(?P<external_id>[\w]{6})/(?P<slug>[\w-]+)", views.resource, name="resource")
-#curated_resource_pattern = url(r"^(?P<pk>[\w-]+)/", include(resource_patterns))
+resource_patterns = [
+    url(r"^$", views.ResourceView.as_view(), name="resource"),
+    url(r"^/edit$", views.ResourceEditView, name="resource edit"),
+]
 
 category_patterns = [
-    url(r"^$", views.category, name="category"),
-    resource_by_pk_pattern,
+    url(r"^$", views.CategoryLandingView.as_view(), name="category landing"),
+    url(r"^(?P<external_id>[\w]{6})/(?P<slug>[\w-]+)", include(resource_patterns))
 ]
 
 urlpatterns = [
-    url(r"^$", views.resources, name="resources"),
-    url(r"^risk-assessments/", include(category_patterns), {"category": "riskassessments"}),
-    url(r"^activities/", include(category_patterns), {"category": "activities"}),
-    url(r"^meetings/", include(category_patterns), {"category": "meetings"}),
-    url(r"^camps/", include(category_patterns), {"category": "camps"}),
-    #curated_resource_pattern
+    url(r"^$", views.ResourceCentreLandingView.as_view(), name="resource centre landing"),
+    url(r"^risk-assessments/", include(category_patterns), {"category": "RiskAssessment"}),
+    url(r"^activities/", include(category_patterns), {"category": "Activity"}),
+    url(r"^meetings/", include(category_patterns), {"category": "Meeting"}),
+    url(r"^camps/", include(category_patterns), {"category": "Camp"}),
 ]
