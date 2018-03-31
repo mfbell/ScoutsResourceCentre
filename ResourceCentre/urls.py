f61c41ab619b089ae3c87bba2426505f84ac9df0
mtech <kind.of.random@outlook.com>
2018-03-31 19:18:53 +0100

    Old templates moved

--- ResourceCentre/urls.py
+++ ResourceCentre/urls.py
@@ -5,23 +5,9 @@ from . import views
 
 app_name = 'ResourceCentre'
 
-# To do
-# - add sitemap
-
-resource_patterns = [
-    path("", views.resource_view_divider, name="resource"),
-    path("edit", views.GenericResourceEdit.as_view(), name="resource-edit"),
-]
-
-category_patterns = [
-    path("", views.CategoryLandingView.as_view(), name="category-landing"),
-    path("<str:external_id>/<slug:slug>/", include(resource_patterns))
-]
-
 urlpatterns = [
-    path("", views.ResourceCentreLandingView.as_view(), name="resource-centre-landing"),
-    path("risk-assessments/", include(category_patterns), {"category": "RiskAssessment"}),
-    path("activities/", include(category_patterns), {"category": "Activity"}),
-    path("meetings/", include(category_patterns), {"category": "Meeting"}),
-    path("camps/", include(category_patterns), {"category": "Camp"}),
+    path("", views.homepage, name="homepage"),
+    path("resources/", views.resources, name="resources"),
+    path("resources/<slug:slug>", views., name="resource"),
+    path("create", views.create, name="create"),
 ]
--- ResourceCentre/views.py
+++ ResourceCentre/views.py
@@ -6,108 +6,24 @@ from . import models
 from . import forms
 
 
-def suggested_resources(user, quantity, category=[], **kwargs):
-    return []
+def homepage(request):
+    return render(request, "ResourceCentre/homepage.html")
 
-def related_resources(resource, quantity, category=[], **kwargs):
-    return []
+def resources(request):
+    return render(request, "ResourceCentre/resources.html")
 
-class ResourceCentreLandingView(View):
-    """Resource centre laning page view."""
+def create(request):
+    return render(request, "ResourceCentre/create.html")
 
-    def get(self, request):
-        # To add
-        # - Suggested content
-        return render(request, "ResourceCentre/landing.html")
-
-
-class CategoryLandingView(View):
-    """Resource category landing page."""
-
-    def get(self, request, category):
-        # To add
-        # - search system? or suggested content
-        return render(request, "ResourceCentre/category-landing.html", {"category": category})
-
-
-class ViewDivider:
-    """Mutli-view selection handler."""
-
-    def __init__(self, keyword, options, delete_kw=True):
-        """
-        keyword - kwarg name | string
-        options - key-value pair of kwarg value and view | dictionary
-        delete_kw - Delete keyword pair from kwargs
-
-        """
-        self.keyword = keyword
-        self.options = options
-        self.delete_kw = delete_kw
-
-    def __call__(self, request, *args, **kwargs):
-        choice = kwargs[self.keyword]
-        if self.delete_kw:
-            del(kwargs[self.keyword])
-        return self.options[choice](request, *args, **kwargs)
-
-
-class ActivityView(View):
-    template = "ResourceCentre/resources/activity.html"
-
-    def get(self, request, external_id, slug):
-        resource = get_object_or_404(models.Activity, external_id=external_id, slug=slug)
-        related = []
-        return render(request, self.template, {"resource": resource, "related": related_resources})
-
-
-class MeetingView(View):
-    template = "ResourceCentre/resources/meeting.html"
-
-    def get(self, request, external_id, slug):
-        resource = get_object_or_404(models.Meeting, external_id=external_id, slug=slug)
-        related = []
-        return render(request, self.template, {"resource": resource, "related": related_resources})
-
-
-class CampView(View):
-    template = "ResourceCentre/resources/camp.html"
-
-    def get(self, request, external_id, slug):
-        resource = get_object_or_404(models.Camp, external_id=external_id, slug=slug)
-        related = []
-        return render(request, self.template, {"resource": resource, "related": related_resources})
-
-
-class RiskAssessmentView(View):
-    template = "ResourceCentre/resources/risk-assessment.html"
-
-    def get(self, request, external_id, slug):
-        resource = get_object_or_404(models.RiskAssessment, external_id=external_id, slug=slug)
-        related = []
-        return render(request, self.template, {"resource": resource, "related": related_resources})
-
-
-resource_view_divider = ViewDivider(
-    "category",
-    {
-        "Activity": ActivityView.as_view(),
-        "Meeting": MeetingView.as_view(),
-        "Camp": CampView.as_view(),
-        "RiskAssessment": RiskAssessmentView.as_view()
+class Resource(View):
+    templates = {
+        "activity": "ResourceCentre/activity.html",
+        "risk-assessment": "ResourceCentre/risk-assessment.html"
     }
-)
 
+    def get(self, request, slug):
+        resource = get_object_or_404(models.Resource, slug=slug)
+        return render(request, self.templates[resource.type], {"resource": resource})
 
-class GenericResourceEdit(View):
-    template = "ResourceCentre/resources/edit/generic.html"
-
-    def get(self, request, category, external_id, slug):
-        form = forms.ActivityForm()
-        return render(request, self.template, {"form": form})
-
-    def post(self, request, category, external_id, slug):
-        form = forms.ActivityForm(request.POST)
-        if form.is_valid():
-            #
-            return HttpResponseRedirect('./')
-        return render(request, self.template, {"form": form})
+    def post(self, request, slug):
+        pass
