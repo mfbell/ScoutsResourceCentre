2684258ff02647abafb2a390c815a024e42d8471
mtech <kind.of.random@outlook.com>
2018-04-02 21:31:50 +0100

    reverse names

--- ScoutsResourceCentre/urls.py
+++ ScoutsResourceCentre/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("ResourceCentre.urls")),
+    path("", include("ResourceCentre.urls"), name="ResourceCentre"),
     #path("badges/", include("BadgeInfo.urls")),
 ]
