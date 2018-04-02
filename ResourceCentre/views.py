44c0960fe24919c74501d29f1e22283acbb9a112
mtech <kind.of.random@outlook.com>
2018-04-02 21:30:34 +0100

    work on views

--- ResourceCentre/views.py
+++ ResourceCentre/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.views import View
+from django.urls import reverse
 
 from . import models
 from . import forms
@@ -25,5 +26,56 @@ class Resource(View):
         resource = get_object_or_404(models.ResourceSlug, slug=slug).resource
         return render(request, self.templates[type(resource)], {"resource": resource})
 
-    def post(self, request, slug):
+
+class EditActivity(View):
+    template = "ResourceCentre/edit-resource.html"
+
+    def get(self, request, slug, resource):
+        activity_form = forms.ActivityForm(instance=resource)
+        activitystage_formset = forms.ActivityStageInlineFormSet(instance=resource)
+        return render(
+            request,
+            self.template,
+            {
+                "activity_form": activity_form,
+                "activitystage_formset": activitystage_formset
+            }
+        )
+
+    def post(self, request, slug, resource):
+        activity_form = forms.ActivityForm(request.POST, instance=resource)
+        activitystage_formset = forms.ActivityStageInlineFormSet(request.POST, instance=resource)
+        if activity_form.is_valid() and activitystage_formset.is_valid():
+            activity_form.save()
+            activitystage_formset.save()
+            return HttpResponseRedirect(reverse("ResourceCentre:resource", args=(slug,)))
+
+        return render(
+            request,
+            self.template,
+            {
+                "activity_form": activity_form,
+                "activitystage_formset": activitystage_formset
+            }
+        )
+
+
+class EditRiskAssessment(View):
+    template = "ResourceCentre/edit-resource.html"
+
+    def get(self, request, slug, resource):
+        pass
+
+    def post(self, request, slug, resource):
+        pass
+
+
+class EditResourceSplitter:
+    views = {
+        models.Activity: EditActivity.as_view(),
+        models.RiskAssessment: EditRiskAssessment.as_view()
+    }
+
+    def __call__(self, request, slug):
         resource = get_object_or_404(models.ResourceSlug, slug=slug).resource
+        return self.views[type(resource)](request, slug, resource)
