5cc0b6ba95a681790e9616389930467d70a4cff5
mtech <kind.of.random@outlook.com>
2018-04-02 21:28:26 +0100

    Work to forms and formsets

--- ResourceCentre/forms.py
+++ ResourceCentre/forms.py
@@ -1,34 +1,46 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, inlineformset_factory
 
 from . import models
-# Activity, Meeting, Camp, RiskAssessmentElement, RiskAssessment
 
-"""
+
 class ActivityForm(ModelForm):
     class Meta:
         model = models.Activity
         fields = [
             "title",
-            "visibility",
-            "desciption",
+            "description",
             "beavers",
             "cubs",
             "scouts",
             "explorers",
             "network",
-            "plan",
-            "resources",
-            "equipment",
-            #"risk_assessment",
-            "location",
+            "curated",
+            "complete",
             "leaders",
             "young_leaders",
-            "young_people",
-            "young_people_approx",
-            "size",
-            "inside_outside",
-            "cost_per_young_person",
-            "cost_approx",
-            "duration"
+            "location",
+            "young_people_low",
+            "young_people_high",
+            "activity_size",
+            "inside_outside"
         ]
-"""
+
+
+class ActivityStageForm(ModelForm):
+    class Meta:
+        model = models.ActivityStage
+        fields = [
+            "title",
+            "description",
+            "equipment",
+            "risks"
+        ]
+
+
+ActivityStageInlineFormSet = inlineformset_factory(
+    models.Activity,
+    models.ActivityStage,
+    form=ActivityStageForm,
+    min_num=1,
+    extra=0
+)
