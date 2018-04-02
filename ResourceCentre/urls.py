f0668b71442025c47c84f28426ebf70701b2c0ab
mtech <kind.of.random@outlook.com>
2018-04-02 21:29:18 +0100

    small changes

--- ResourceCentre/urls.py
+++ ResourceCentre/urls.py
@@ -8,6 +8,7 @@ app_name = 'ResourceCentre'
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("resources/", views.resources, name="resources"),
-    path("resources/<slug:slug>", views.Resource.as_view(), name="resource"),
+    path("resources/<slug:slug>/", views.Resource.as_view(), name="resource"),
+    path("resources/<slug:slug>/edit", views.EditResourceSplitter(), name="edit-resource"),
     path("create", views.create, name="create"),
 ]
